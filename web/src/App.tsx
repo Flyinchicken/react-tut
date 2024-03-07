8ad4ea2120b28f91a1ed1397668e008cdbec59fc
Weijie Tan <twj3454@hotmail.com>
2024-03-07 14:08:25 -0500

    added reset button

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,11 +6,15 @@ function App() {
   function handleClick() {
     setCount(count + 1);
   }
+  function resetCount() {
+    setCount(0);
+  }
 
   return <div>
     <h1>This is a count app!</h1>
     <h2>Current count {count}</h2>
     <button onClick={handleClick}>Click Me!</button>
+    <button onClick={resetCount}> Reset </button>
   </div>;
 }
 
